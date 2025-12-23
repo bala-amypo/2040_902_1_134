73474389a5c855e2c4c67ef6b7bc987412207971
bala-amypo <bala@amypo.in>
2025-12-23 07:02:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviationRuleServiceImpl.java
@@ -1,28 +1,13 @@
-package com.service.impl;
+package com.example.demo.service.impl;
 
-import com.example.demo.model.DeviationRule;
-import com.example.demo.repository.DeviationRuleRepository;
 import com.example.demo.service.DeviationRuleService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DeviationRuleServiceImpl implements DeviationRuleService {
 
-    private final DeviationRuleRepository repository;
-
-    public DeviationRuleServiceImpl(DeviationRuleRepository repository) {
-        this.repository = repository;
-    }
-
     @Override
-    public DeviationRule createRule(DeviationRule rule) {
-        return repository.save(rule);
-    }
-
-    @Override
-    public List<DeviationRule> getAllRules() {
-        return repository.findAll();
+    public String getMessage() {
+        return "Deviation Rule Service is working";
     }
 }
