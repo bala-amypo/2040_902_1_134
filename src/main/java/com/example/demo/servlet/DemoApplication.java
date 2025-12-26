d4ca04f84654439d962994e9fa917c0e1a641ebb
bala-amypo <bala@amypo.in>
2025-12-26 16:43:08 +0000

    User commit

--- src/main/java/com/example/demo/servlet/DemoApplication.java
+++ src/main/java/com/example/demo/servlet/DemoApplication.java
@@ -3,12 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void onApplicationReady() {
+		System.out.println("\n=== APPLICATION IS READY AND RUNNING ===\n");
 	}
 
 }
