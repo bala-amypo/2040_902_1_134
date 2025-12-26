0c6f9aa8d0654dfa21403b86733fce8bcbb5b8df
bala-amypo <bala@amypo.in>
2025-12-26 08:41:27 +0000

    User commit

--- src/main/java/com/example/demo/service/ClinicalAlertService.java
+++ src/main/java/com/example/demo/service/ClinicalAlertService.java
@@ -1,10 +1,19 @@
 package com.example.demo.service;
 
 import com.example.demo.model.ClinicalAlertRecord;
+
 import java.util.List;
 
 public interface ClinicalAlertService {
+
     ClinicalAlertRecord createAlert(ClinicalAlertRecord alert);
-    List<ClinicalAlertRecord> getAlertsForPatient(Long patientId);
+
     List<ClinicalAlertRecord> getAllAlerts();
+
+    ClinicalAlertRecord getAlertById(Long id);
+
+    void deleteAlert(Long id);
+
+    // THIS MUST BE HERE, because your service impl references it
+    List<ClinicalAlertRecord> getAlertsForPatient(Long patientId);
 }
